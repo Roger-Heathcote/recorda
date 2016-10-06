57b027392d52afa98035d1d484e4373c4c063ef6
Roger Heathcote <git@technicalbloke.com>
2016-10-07 00:47:57 +0100

    Tidying of AudioEngine.js

--- scripts/AudioEngine.js
+++ scripts/AudioEngine.js
@@ -14,7 +14,7 @@ var AudioEngine = function (GLOBALS, loResWaveformParams=false) {
       console.log( "Total audio blocks handled so far:", this.totalBlocksHandled );
      }
   };
-  //this.state = "buffer";
+
   this.scriptProcessorBuffer = 16384 / 4; //64;
   this.channels = 2;
   this.audioContext = new (GLOBALS.win.AudioContext || GLOBALS.win.webkitAudioContext)();
@@ -33,7 +33,6 @@ var AudioEngine = function (GLOBALS, loResWaveformParams=false) {
   this.rightChannel = new Array(this.recBufArrayLength).fill(0);
   this.codeChannel = new Array(this.recBufArrayLength).fill(0);
 
-let samples =
   console.log("Should have:", this.leftChannel.length * 60 * this.scriptProcessorBuffer, "");
 
   this.codeNumber = 0;
@@ -61,54 +60,36 @@ let samples =
 
   if (GLOBALS.nav.getUserMedia) {
     console.log('getUserMedia supported.');
-
-
-
     GLOBALS.nav.getUserMedia (
-      // Ask for audio input
       { audio: true },
-      // If we get it...
-      function(stream) {
-        console.log("IN THE STREAM FUNCTION");
-        var source = temp.audioContext.createMediaStreamSource(stream);
-        source.connect(temp.scriptNode);
-        temp.scriptNode.connect(temp.gainNode);
-        temp.gainNode.connect(temp.audioContext.destination);
-        //updateDisplay();
-      },
-      // If we don't
-      function(err) {
-        console.log('The following gUM error occured: ' + err);
+      function(audioStream) {
+        var source = this.audioContext.createMediaStreamSource(audioStream);
+        source.connect(this.scriptNode);
+        this.scriptNode.connect(this.gainNode);
+        this.gainNode.connect(this.audioContext.destination);
+      }.bind(this),
+      function(streamError) {
+        throw( new Error("The following gUM Error occured: " + streamError) );
       }
     );
   } else {
-     console.log('getUserMedia not supported on your browser!');
+     throw( new Error("getUserMedia not supported on your browser!") );
   }
 
   this.scriptNode.onaudioprocess = function(audioProcessingEvent) {
     this.codeNumber++;
-    //console.log(this.codeNumber);
     let left = audioProcessingEvent.inputBuffer.getChannelData (0);
     let right = audioProcessingEvent.inputBuffer.getChannelData (1);
     let leftout = audioProcessingEvent.outputBuffer.getChannelData (0);
     let rightout = audioProcessingEvent.outputBuffer.getChannelData (1);
-
-    // we clone the samples
-    // TOD
     this.leftChannel.push (new Float32Array (left));
     this.rightChannel.push (new Float32Array (right));
     this.codeChannel.push (this.codeNumber);
 
     for (let sample = 0; sample < this.scriptProcessorBuffer; sample++) {
-
-      // Looks like we didn't have to do this :)
-      // leftout[sample] = left[sample];
-      // rightout[sample] = right[sample];
-
       // track max amplitude encountered
       if (left[sample] > this.dispPeak) { this.dispPeak = left[sample]; }
       if (left[sample] < -this.dispPeak) { this.dispPeak = -left[sample]; } // TODO check both
-
       if(loResWaveformParams){
         // if enough samples have elapsed push a display data point & reset counter
         this.dispCount--;
@@ -119,26 +100,23 @@ let samples =
           this.loResCodeChannel.push(this.codeNumber);
           this.loResCodeChannel.shift();
           this.dispPeak = 0;
-
           if(!this.loResOffset) {
             this.loResOffset = this.codeNumber - 1;
           }
-
         }
       } //end if
-
     } // end for
-    //if not this.recording drop block from front of buffer
+
     while ((GLOBALS.state === "buffer") && (this.leftChannel.length > this.recBufArrayLength)) {
       let trimLength = this.leftChannel.length - this.recBufArrayLength;
       console.log(trimLength," to trim");
-      //this.leftChannel.shift(trimLength); this.rightChannel.shift(trimLength); this.codeChannel.shift(trimLength);
       this.leftChannel.splice(0, trimLength);
       this.rightChannel.splice(0, trimLength);
       this.codeChannel.splice(0, trimLength);
     }
 
-    temp.updateBlockTotal();
+    this.updateBlockTotal();
+
   }.bind(this);
 
 
