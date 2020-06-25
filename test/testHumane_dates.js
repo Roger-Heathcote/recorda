dff7482a3937577464a55fe8fdb2ade85f269519
Roger Heathcote <git@technicalbloke.com>
2020-06-25 18:18:35 +0100

    Rewrote humaneDate and it's tests as they were both ugly and overcomplicated.

--- test/testHumane_dates.js
+++ test/testHumane_dates.js
@@ -3,93 +3,56 @@
 require("./helpers/all.js");
 
 let humaneDate = require("../source/humane_dates").humaneDate;
-describe("Let's test humane dates", function(){
+describe("Let's test humane dates", function () {
+    it("Should throw when passed nothing", function () {
+        expect(() => humaneDate("justone")).to.throw();
+    });
 
-  let localCurrentDateObject = function(secondsAgo=0){
-    let dateObj = new Date();
-    let tzOffset = dateObj.getTimezoneOffset() * 6000;
-    let dn = new Date((Date.now() - tzOffset) - (secondsAgo*1000));
-    return dn;
-  };
+    it("Should say '<1m ago' if time diff is under 60s", function () {
+        expect(humaneDate(0, 59 * 1000)).to.equal("<1m ago");
+    });
 
-  it("Should throw when passed nothing", function(){
-    expect(humaneDate).to.throw();
-  });
+    it("Should say '1m ago' if difference is 61s", function () {
+        expect(humaneDate(60 * 1000, 121 * 1000)).to.equal("1m ago");
+    });
 
-  it("Should say '<1m ago' if called with current time", function(){
-    let dn = localCurrentDateObject();
-    expect(humaneDate(dn)).to.equal("<1m ago");
-  });
+    it("Should say '1m ago' if difference is 119s", function () {
+        expect(humaneDate(0, 119 * 1000)).to.equal("1m ago");
+    });
 
-  it("Should say '<1m ago' if called with current time - 30 seconds", function(){
-    let dn = localCurrentDateObject(30);
-    expect(humaneDate(dn)).to.equal("<1m ago");
-  });
+    it("Should say '2m ago' if difference is 120s", function () {
+        expect(humaneDate(0, 120 * 1000)).to.equal("2m ago");
+    });
 
-  it("Should say '1m ago' if called with current time - 65 seconds", function(){
-    let dn = localCurrentDateObject(65);
-    expect(humaneDate(dn)).to.equal("1m ago");
-  });
+    it("Should say '59m ago' if diff is 1ms shy of 1hr", function () {
+        expect(humaneDate(0, 59 * 60 * 1000 + 59 * 1000 + 999)).to.equal("59m ago");
+    });
 
-  it("Should say '59m ago' if called with current time - 59m + 59s seconds", function(){
-    let dn = localCurrentDateObject((60*58)+59);
-    expect(humaneDate(dn)).to.equal("59m ago");
-  });
+    it("Should say '1h ago' if called with 1hr diff", function () {
+        expect(humaneDate(0, 60 * 60 * 1000)).to.equal("1h ago");
+    });
 
-  it("Should say '1hr ago' if called with current time - 1hr 5s", function(){
-    let dn = localCurrentDateObject((60*60)+5);
-    expect(humaneDate(dn)).to.equal("1hr ago");
-  });
+    it("Should say '23h ago' if diff is 1ms shy of 24hrs", function () {
+        expect(humaneDate(0, 24 * 60 * 60 * 1000 - 1)).to.equal("23h ago");
+    });
 
-  it("Should say '1hr ago' if called with current time - 1hr 5m", function(){
-    let dn = localCurrentDateObject((60*60)+(5*60));
-    expect(humaneDate(dn)).to.equal("1hr ago");
-  });
+    it("Should say '3d ago' if called with exactly 3 days diff", function () {
+        expect(humaneDate(0, 3 * 24 * 60 * 60 * 1000)).to.equal("3d ago");
+    });
 
-  it("Should say '1hr ago' if called with current time - 1hr 6m", function(){
-    let dn = localCurrentDateObject((60*60)+(6*60));
-    expect(humaneDate(dn)).to.equal("1hr ago");
-  });
+    it("Should say '1w ago' if called with 1w diff", function () {
+        expect(humaneDate(0, 7 * 24 * 60 * 60 * 1000)).to.equal("1w ago");
+    });
 
-  it("Should say '1hr ago' if called with current time - 1hr 10m", function(){
-    let dn = localCurrentDateObject((60*60)+(10*60));
-    expect(humaneDate(dn)).to.equal("1hr ago");
-  });
+    it("Should say '51w ago' if called with 1y - 1ms diff", function () {
+        expect(humaneDate(0, 52 * 7 * 24 * 60 * 60 * 1000 - 1)).to.equal("51w ago");
+    });
 
-  it("Should say '1hr ago' if called with current time - 1hr 15m", function(){
-    let dn = localCurrentDateObject((60*60)+(15*60));
-    expect(humaneDate(dn)).to.equal("1hr ago");
-  });
+    it("Should say '1y ago' if called with 1y", function () {
+        expect(humaneDate(0, 52 * 7 * 24 * 60 * 60 * 1000)).to.equal("1y ago");
+    });
 
-  it("Should say '1hr ago' if called with current time - 1hr 25m", function(){
-    let dn = localCurrentDateObject((60*60)+(25*60));
-    expect(humaneDate(dn)).to.equal("1hr ago");
-  });
-
-  it("Should say '1hr ago' if called with current time - 2hrs -1s", function(){
-    let dn = localCurrentDateObject((2*60*60)-1);
-    expect(humaneDate(dn)).to.equal("1hr ago");
-  });
-
-  // TODO these are wrong, replace this damn module!
-
-  // it("Should say '2hrs ago' if called with current time - 3hrs -1s", function(){
-  //   let dn = localCurrentDateObject((3*60*60)-1);
-  //   expect(humaneDate(dn)).to.equal(" ago");
-  // });
-
-  // it("Should say '3hrs ago' if called with current time - 3hr 5s", function(){
-  //   let dn = localCurrentDateObject((3*60*60)+5);
-  //   expect(humaneDate(dn)).to.equal("3hrs ago");
-  // });
-
-  // it("Should say '23day ago' if called with current time - 25hrs", function(){
-  //   let dn = localCurrentDateObject((23*60*60));
-  //   expect(humaneDate(dn)).to.equal("1day ago");
-  // });
-
-  it("Should say '1day ago' if called with current time - 25hrs", function(){
-    let dn = localCurrentDateObject((25*60*60));
-    expect(humaneDate(dn)).to.equal("1day ago");
-  });
+    it("Should say '1234y ago' if called with 1234y difference", function () {
+        expect(humaneDate(0, 1234 * 52 * 7 * 24 * 60 * 60 * 1000)).to.equal("1234y ago");
+    });
 });
