1d73132c604e9142b15412379945d695df2f5bde
Roger Heathcote <git@technicalbloke.com>
2020-06-30 22:31:45 +0100

    removed devtool

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,21 @@
-let HtmlWebpackPlugin = require('html-webpack-plugin');
-let WebpackBuildNotifierPlugin = require('webpack-build-notifier');
+let HtmlWebpackPlugin = require("html-webpack-plugin");
+let WebpackBuildNotifierPlugin = require("webpack-build-notifier");
 
 module.exports = {
-  entry: './source/app.js',
-  output: {
-    filename: 'webpackbundle.js',
-    path: './build'
-  },
-  devtool: "source-map",
-  plugins: [
+    entry: "./source/app.js",
+    output: {
+        filename: "webpackbundle.js",
+        path: "./build"
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "source/index.html",
+            inject: "body"
+        }),
 
-    new HtmlWebpackPlugin({
-      template: 'source/index.html',
-      inject: 'body'
-    }),
-
-    new WebpackBuildNotifierPlugin({
-      title: "My Project Webpack Build",
-      suppressSuccess: false
-    })
-
-  ]
+        new WebpackBuildNotifierPlugin({
+            title: "My Project Webpack Build",
+            suppressSuccess: false
+        })
+    ]
 };
